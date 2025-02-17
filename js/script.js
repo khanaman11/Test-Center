c2bc0c729ce07b5539ca7a9ca028fa206f356fd9
Aman khan <amanullahkhan11595@gmail.com>
2025-02-17 14:55:45 +0530

    second code push

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 
+// ////////////////////////// Pagr next and Previous script start //////////////////////////////////////////
+
+function showPage(pageNumber) {
+    // Sab pages hide kar do
+    document.querySelectorAll(".page").forEach(page => {
+        page.classList.remove("active");
+    });
+
+    // Sirf current page dikhana hai
+    document.getElementById(`page${pageNumber}`).classList.add("active");
+}
+
+// ////////////////////////// Pagr next and Previous script start //////////////////////////////////////////
